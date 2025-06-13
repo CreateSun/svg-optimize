acc1a437bb2baa0af4c79fc692deea7ba20c501d
yucui <yucui@trip.com>
2025-06-13 21:17:56 +0800

    commit page

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,10 @@
 export default function Sitemap() {
   return [
     {
+      url: "https://svg-optimizer.com/zh",
+      lastModified: new Date(),
+    },
+    {
       url: "https://svg-optimizer.com",
       lastModified: new Date(),
     },
