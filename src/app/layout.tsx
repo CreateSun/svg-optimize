69ca1b3d0d9b0bcbff6a21be9b0c677f63b79f88
yucui <yucui@trip.com>
2025-06-23 20:34:48 +0800

    feat update baidu verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,18 @@
 import Script from "next/script";
 import "./globals.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  other: {
+    "baidu-site-verification": "codeva-YqdLRGZgt8",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-
   return (
     <html lang={"en"}>
       <body>{children}</body>
