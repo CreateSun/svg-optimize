ed8e24e9f6c9c40652655e1688600c175f4092a2
yucui <yucui@trip.com>
2025-06-24 22:58:57 +0800

    Feat update tdk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,15 @@ import { Metadata } from "next";
 import { HomePage } from "./components/home";
 
 export const metadata: Metadata = {
-  title: "Free Online Safe SVG Optimizer",
-  description: "A free online safe SVG optimizer that can optimize your SVG files and make them smaller and faster.",
+  title: "SVG Optimizer - Free Online Safe SVG Optimizer use SVGO Online",
+  description: "A free online safe SVG optimizer that can optimize your SVG files using SVGO Online and make them smaller and faster.",
   icons: {
     icon: "/favicon.ico",
   },
-  keywords: ["Data safe SVG Optimizer", "Free Online Safe SVG Optimizer", "Free Online Safe SVG Optimizer", "Free Online Safe SVG Optimizer"],
+  keywords: ["SVG Optimizer", "SVGO Online Optimizer",  "Data safe SVG Optimizer", "Free Online Safe SVG Optimizer", "Free Online Safe SVG Optimizer", "Free Online Safe SVG Optimizer"],
   openGraph: {
-    title: "Free Online Safe SVG Optimizer",
-    description: "A free online safe SVG optimizer that can optimize your SVG files and make them smaller and faster.",
+    title: "SVG Optimizer - Free Online Safe SVG Optimizer use SVGO Online",
+    description: "A free online safe SVG optimizer that can optimize your SVG files using SVGO Online and make them smaller and faster.",
     images: ["/favicon.ico"],
   },
 };
--- src/app/zh/page.tsx
+++ src/app/zh/page.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
     icons: {
       icon: "/favicon.ico",
     },
-    keywords: ["免费SVG压缩工具", "在线SVG压缩工具", "数据安全的SVG压缩工具", "企业级SVG压缩工具"],
+    keywords: ["免费SVG压缩工具", "SVG无损压缩", "SVG代码压缩", "在线SVG压缩工具", "数据安全的SVG压缩工具", "企业级SVG压缩工具"],
     openGraph: {
       title: "免费在线安全SVG压缩工具",
       description: "一个免费的在线SVG压缩工具，可以压缩您的SVG文件并使其更小， 全程数据不落地，保护您的数据安全，适合企业级使用。",
